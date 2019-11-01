786c1ea41571498c4d1db75093a8475ade59a89e
JianFei.Wang <jiucheng0605@gmail.com>
2019-11-01 17:55:36 +0800

    使用atMessage顶部提示消息组件

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -1,7 +1,7 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
-import { AtForm, AtInput, AtButton } from 'taro-ui'
+import { AtForm, AtInput, AtButton, AtMessage } from 'taro-ui'
 
 import { add, minus, asyncAdd } from '../../actions/counter'
 
@@ -28,6 +28,15 @@ class Register extends Component {
     navigationBarTitleText: '注册'
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      userName: '',
+      passWord: '',
+      rePassWord: '',
+    }
+  }
+
   componentWillReceiveProps(nextProps) {
     console.log(this.props, nextProps)
   }
@@ -38,29 +47,85 @@ class Register extends Component {
 
   componentDidHide() { }
 
+  changeNameHandle(value) {
+    this.setState({
+      userName: value,
+    })
+  }
+
+  changePasswordHandle(value) {
+    this.setState({
+      passWord: value,
+    })
+  }
+
+  changeRePasswordHandle(value) {
+    this.setState({
+      rePassWord: value,
+    })
+  }
+
+  submitRegHandle = () => {
+    const { userName, passWord, rePassWord } = this.state
+    if (!userName || !passWord) {
+      return Taro.atMessage({
+        'message': '用户名或密码不能为空',
+        'type': 'warning',
+      })
+    }
+    if (!rePassWord) {
+      return Taro.atMessage({
+        'message': '请再次确认密码',
+        'type': 'warning',
+      })
+    }
+    if (passWord && rePassWord && (passWord !== rePassWord)) {
+      return Taro.atMessage({
+        'message': '两次密码输入的不一致',
+        'type': 'warning',
+      })
+
+    }
+    if (userName && passWord && rePassWord && (passWord === rePassWord)) {
+      Taro.atMessage({
+        'message': '注册成功',
+        'type': 'success',
+      })
+      console.log(`注册用户提交`)
+    }
+  }
+
   render() {
+    const { userName, passWord, rePassWord } = this.state
     return (
       <View className='register'>
-        <AtForm>
+        <AtMessage />
+        <AtForm onSubmit={this.submitRegHandle}>
           <AtInput
             name='userName'
+            value={userName}
+            onChange={this.changeNameHandle.bind(this)}
             placeholder='请输入用户名'
             title='用户名'
             type='text'
           />
           <AtInput
             name='passWord'
+            value={passWord}
+            onChange={this.changePasswordHandle.bind(this)}
             placeholder='请输入密码'
             title='密码'
             type='password'
           />
           <AtInput
             name='RepassWord'
+            value={rePassWord}
+            onChange={this.changeRePasswordHandle.bind(this)}
             placeholder='请再次输入密码'
             title='确认密码'
             type='password'
           />
-          <AtButton type='primary'>注册</AtButton>
+          <AtButton formType='submit' type='primary'>注册</AtButton>
         </AtForm>
         <Text onClick={() => Taro.redirectTo({ url: '/pages/auth/auth' })} className='tips-btn'>已有账号，立即登录</Text>
       </View>
