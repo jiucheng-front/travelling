7fc4efb8a06649cd491fd7dfd4ab26ebb1eee797
JianFei.Wang <jiucheng0605@gmail.com>
2019-12-03 16:37:34 +0800

    add description &

--- src/pages/article/article.jsx
+++ src/pages/article/article.jsx
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Button, Text } from '@tarojs/components'
+import { View, Text } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
 
 import { add, minus, asyncAdd } from '../../actions/counter'
